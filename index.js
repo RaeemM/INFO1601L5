5bad67e769b7a8a572eafe59ec1ba3ca2644179d
Raeem Mohammed <raeemmohammed@gmail.com>
2025-03-03 18:14:27 -0400

    Update index.js
    Task 4.1

--- index.js
+++ index.js
@@ -22,3 +22,12 @@ console.log(`Hello my name is ${name}, I'm ${age} years old`);
 
 console.log(`I was born in ${2020 - age}`)
 //I was born in 1966
+
+/*Task 4.1*/
+console.log('1' == 1); //true
+console.log(1 == true); //true
+console.log("false" == false); // "false" is not false
+console.log("false" == true); // "false" is not true either
+
+if("false")
+   console.log("Hello false!") //but "false" is true enough here
