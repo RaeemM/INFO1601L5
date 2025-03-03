1586b9448d37c8c760b7aded4e5a12f9d1d465ae
Raeem Mohammed <raeemmohammed@gmail.com>
2025-03-03 19:22:54 -0400

    Update index.js
    Task 5

--- index.js
+++ index.js
@@ -49,3 +49,23 @@ for(let i=0; i< 50; i++){
     console.log(i);
   }
 }
+
+/*Task 5*/
+function happyPrint(string){
+  console.log("😀: "+string);
+ }
+ 
+ function sadPrint(string){
+  console.log("😢: "+string);
+ }
+ 
+ //This high-order function adds 2 parameters and passes the answer to the callback
+ function add(a, b, callback){
+    let ans = a + b;
+    callback(ans);// call the callback and pass the answer to it
+ }
+ 
+ //call add passing the callbacks to it
+ 
+ add(5, 10, happyPrint);
+ add(11, 12, sadPrint);
