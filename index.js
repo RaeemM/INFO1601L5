cf4e0cd85e84768171e9e61e1b9af08f84ad83d2
Raeem Mohammed <raeemmohammed@gmail.com>
2025-03-03 18:09:06 -0400

    Update index.js
    Task 3

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ for(let i=0; i< arr.length; i++){
   console.log(arr[i])
 }
 
+/*Task 2*/
 let radius = 7;
 const pi = 3.14;
 
 let area = radius * radius * pi;
+
+/*Task 3*/
+let name = "bob";
+let age = 24;
+
+console.log(typeof(name))//string
+console.log(typeof(age))//number
+
+console.log(`Hello my name is ${name}, I'm ${age} years old`);
+//Hello my name is bob, I'm 24 years old.
+
+console.log(`I was born in ${2020 - age}`)
+//I was born in 1966
