6983052f0378b5b6088af70411940a6395b68222
Raeem Mohammed <raeemmohammed@gmail.com>
2025-03-03 20:02:22 -0400

    Update index.js
    Task 5.1

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+//COMMENTED OUT FOR TASK 5.1
+/*
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
 
 for(let i=0; i< arr.length; i++){
   console.log(arr[i])
 }
-
+*/
 /*Task 2*/
 let radius = 7;
 const pi = 3.14;
@@ -77,4 +79,29 @@ function printDate(){
   console.log(new Date().toLocaleTimeString());
 }
      
-setInterval(printDate, 1000);
+//setInterval(printDate, 1000);
+
+/*Task 5.1*/
+let arr = [ -5, 16, 33, 42, 103, 344];
+
+console.log(arr.includes(-5));//true
+
+arr.push(11);//adds item to the end
+
+console.log(arr)//[ -5, 16, 33, 42, 103, 344, 11];
+
+//HAD TO FIX SYNTAX ERROR HERE.
+let lastItem = arr.pop();//removes last item
+
+console.log(lastItem, arr);//11, [ -5, 16, 33, 42, 103, 344]
+
+arr.unshift(22);//adds item to the front
+
+console.log(arr);//[22, -5, 16, 33, 42, 103, 344]
+
+let firstItem = arr.shift();//removes first item
+console.log(firstItem, arr);//22, [-5, 16, 33, 42, 103, 344]
+
+let reversed = arr.reverse();//creates a new array in reverse order 
+console.log(reversed);//[344, 103, 42, 33, 16, -5]
+console.log(arr.join('-'));//"-5-103-16-33-344-42" joins array with provided separator
