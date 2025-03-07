7ee49c2578441acc8eba3a7c804c87e2123fe361
Raeem Mohammed <raeemmohammed@gmail.com>
2025-03-07 06:01:53 -0400

    Update exercise.js
    getAverateGrade(student, course)

---
+++ exercise.js
@@ -0,0 +1,66 @@
+//object literal
+let bob = {
+    fname: "bob",
+    lname: "smith",
+    age: 18,
+    height: 6,
+    transcript:[
+      {
+        course: 'INFO 1603',
+        grades: [ 89, 34, 67 ]
+      },
+      {
+        course: 'INFO 1601',
+        grades: [ 89, 34, 67 ]
+      }
+    ]
+  };
+  
+  let sally = {
+    fname: "sally",
+    lname: "smith",
+    age: 18,
+    height: 6,
+    transcript:[
+      {
+        course: 'INFO 1601',
+        grades: [ 100, 89, 79 ]
+      }
+    ]
+  };
+  
+  let paul = {
+    fname: "paul",
+    lname: "smith",
+    age: 18,
+    height: 6,
+    transcript:[
+      {
+        course: 'INFO 1600',
+        grades: [ 89, 34, 67 ]
+      }
+    ]
+  };
+  
+  
+  const students = [bob, sally, paul];
+
+  function getAverateGrade(student, course){
+    let sum = 0;
+    for (let tran of student.transcript){
+        if (tran.course === course){
+            for (let grade of tran.grades){
+                sum = sum + grade;
+            }
+        
+            let avg = sum/tran.grades.length;
+            return avg;
+        }
+    }
+
+    return -1;
+  }
+
+  /*Tests*/
+
+  console.log(getAverateGrade(paul, "INFO 1600"));
